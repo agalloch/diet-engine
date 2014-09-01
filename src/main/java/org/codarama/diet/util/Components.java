d40b9f90e374d6e3bcc3cfe347ca93aa4d653cbf
Simeon Iliev <siliev@vmware.com>
2014-09-01 16:55:08 +0300

    - fix horrible formatting

--- src/main/java/org/codarama/diet/util/Components.java
+++ src/main/java/org/codarama/diet/util/Components.java
@@ -3,35 +3,35 @@ package org.codarama.diet.util;
 import org.springframework.context.ApplicationContext;
 
 public enum Components {
-	JAR_MAKER("jarMaker"),
-	LIB_JAR_EXPLODER("jarExploder"),
-	EXPLICIT_JAR_EXPLODER("explicitJarExploder"),
-	CLASS_DEPENDENCY_RESOLVER("classDependencyResolver"),
-	SOURCE_DEPENDENCY_RESOLVER("sourceDependencyResolver"),
-	DEPENDENCY_MATCHER_STRATEGY("unanimousMatcher"),
-	EVENT_BUS("statusUpdateEventBus");
+    JAR_MAKER("jarMaker"),
+    LIB_JAR_EXPLODER("jarExploder"),
+    EXPLICIT_JAR_EXPLODER("explicitJarExploder"),
+    CLASS_DEPENDENCY_RESOLVER("classDependencyResolver"),
+    SOURCE_DEPENDENCY_RESOLVER("sourceDependencyResolver"),
+    DEPENDENCY_MATCHER_STRATEGY("unanimousMatcher"),
+    EVENT_BUS("statusUpdateEventBus");
 
-	private static ApplicationContext CONTEXT;
+    private static ApplicationContext CONTEXT;
 
-	private final String name;
+    private final String name;
 
-	private Components(String name) {
-      if (!isContextInitialized()) {
-         initContext();
-      }
-		this.name = name;
-	}
+    private Components(String name) {
+        if (!isContextInitialized()) {
+            initContext();
+        }
+        this.name = name;
+    }
 
-   private static boolean isContextInitialized() {
-      return CONTEXT != null;
-   }
+    private static boolean isContextInitialized() {
+        return CONTEXT != null;
+    }
 
-   private static void initContext() {
-      CONTEXT = Contexts.SPRING.instance();
-   }
+    private static void initContext() {
+        CONTEXT = Contexts.SPRING.instance();
+    }
 
-	@SuppressWarnings("unchecked")
-	public <T> T getInstance() {
-		return (T) CONTEXT.getBean(name);
-	}
+    @SuppressWarnings("unchecked")
+    public <T> T getInstance() {
+        return (T) CONTEXT.getBean(name);
+    }
 }
